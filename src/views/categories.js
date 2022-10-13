e04701692d99ad4dbd8e3b07d26ec33e8aec4ad0
pray1n <89249367+pray1n@users.noreply.github.com>
2022-10-13 19:18:23 +0200

    Update categories.js

--- src/views/categories.js
+++ src/views/categories.js
@@ -4,8 +4,8 @@ export default function Categories({ categories }) {
     const li_categories = categories.map((category) => {
         return (
             <li key={category.id}>
-                <NavLink className="link" to={category.name}>
-                    {category.name}
+                <NavLink className="link" to={'/frontend-blog-project/' + category.name}>
+                    {category.name} 
                 </NavLink>
             </li>
         )
